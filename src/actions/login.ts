ca6247c48042f9d9d4125619b0cb30b4212eb2fe
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-02-16 00:16:20 +0100

    fixed stuff4

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,15 +1,15 @@
-// 'use server'
+'use server'
 
-// import { LoginSchema } from '@/schemas'
-// import * as z from 'zod'
+import { LoginSchema } from '@/schemas'
+import * as z from 'zod'
 
-// export const login = async (values: z.infer<typeof LoginSchema>) => {
-//   const validatedFields = LoginSchema.safeParse(values)
-//   if (!validatedFields.success) {
-//     return { error: 'Niepoprawne pola' }
-//   }
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values)
+  if (!validatedFields.success) {
+    return { error: 'Niepoprawne pola' }
+  }
 
-//   return {
-//     success: 'Email wysłany!',
-//   }
-// }
+  return {
+    success: 'Email wysłany!',
+  }
+}

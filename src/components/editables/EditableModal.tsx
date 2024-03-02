f73d1d83d60d59338ad4118099819ad988e973a6
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-03-02 14:16:04 +0100

    fixed stuff49

--- src/components/editables/EditableModal.tsx
+++ src/components/editables/EditableModal.tsx
@@ -189,7 +189,7 @@ const EditableModal = ({
   }
 
   return (
-    <div className=" w-full md:max-w-screen-xl h-full fixed left-0 md:left-1/2 md:-translate-x-1/2 top-0 bg-slate-900  py-14 flex flex-col gap-5">
+    <div className=" w-full md:max-w-screen-xl h-full fixed left-0 md:left-1/2 md:-translate-x-1/2 top-0 bg-slate-900  md:py-14 flex flex-col gap-5">
       <div className="w-full flex justify-center flex-col items-center gap-4 mt-24">
         <div className="">Poziom Trudności</div>
 

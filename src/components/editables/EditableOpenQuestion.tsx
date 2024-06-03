7e13cc789fdc19d02da354bac5a8fc270c5e1e85
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-06-03 22:20:05 +0200

    try-4

--- src/components/editables/EditableOpenQuestion.tsx
+++ src/components/editables/EditableOpenQuestion.tsx
@@ -1,13 +1,13 @@
-import { Coins, Lightbulb, Pen, Plus, Timer, X } from 'lucide-react'
-import Image from 'next/image'
-import React, { ChangeEvent, useEffect, useState } from 'react'
-import EditableAnswear from './EditableAnswear'
-import { answearProps, questionsProps } from '@/types/data'
-import { Button } from '../ui/button'
-import { number } from 'zod'
-import { Input } from '../ui/input'
-import { disableTextInInput } from '@/lib/utils'
-import EditableOpenAnswear from './EditableOpenAnswear'
+import { Coins, Lightbulb, Pen, Plus, Timer, X } from "lucide-react";
+import Image from "next/image";
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+import { answearProps, questionsProps } from "@/types/data";
+import { Button } from "../ui/button";
+import { number } from "zod";
+import { Input } from "../ui/input";
+import { disableTextInInput } from "@/lib/utils";
+import EditableOpenAnswear from "./EditableOpenAnswear";
 
 const EditableOpenQuestion = ({
   question,
@@ -17,46 +17,46 @@ const EditableOpenQuestion = ({
   onInput,
   refId,
 }: {
-  question: questionsProps
-  index: string
-  reference: React.RefObject<HTMLDivElement>[]
-  refId: number
-  onDelete: (index: string, refId: number, questionPoints: number) => void
-  onInput: (index: number, time: number, points: number) => void
+  question: questionsProps;
+  index: string;
+  reference: React.RefObject<HTMLDivElement>[];
+  refId: number;
+  onDelete: (index: string, refId: number, questionPoints: number) => void;
+  onInput: (index: number, time: number, points: number) => void;
 }) => {
-  const ref = reference[refId]
-  console.log(question)
-  const [answears, setAnswears] = useState<answearProps[]>(question.answears)
+  const ref = reference[refId];
+  console.log(question);
+  const [answears, setAnswears] = useState<answearProps[]>(question.answears);
 
-  const [image, setImage] = useState<string | null>(question.img)
+  const [image, setImage] = useState<string | null>(question.img);
 
-  const [questionPoints, setQuestionPoints] = useState(question.points)
-  const [questionTime, setQuestionTime] = useState(question.time)
+  const [questionPoints, setQuestionPoints] = useState(question.points);
+  const [questionTime, setQuestionTime] = useState(question.time);
 
   const showImage = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
-      setImage(URL.createObjectURL(event.target.files[0]))
+      setImage(URL.createObjectURL(event.target.files[0]));
     }
-  }
+  };
 
   const addNewAnswear = () => {
     const newAnswear = {
-      title: 'Odpowiedź',
+      title: "Odpowiedź",
       isCorrect: true,
-      id: '',
-    }
-    setAnswears([...answears, newAnswear])
-  }
+      id: "",
+    };
+    setAnswears([...answears, newAnswear]);
+  };
 
   return (
     <div
       key={index}
       ref={ref}
       id={index}
-      className={`w-full bg-slate-950 col-span-2  p-4 flex justify-evenly items-center text-center flex-col rounded-xl gap-3 border-2 relative`}
+      className={`relative col-span-2 flex w-full flex-col items-center justify-evenly gap-3 rounded-xl border-2 bg-slate-950 p-4 text-center`}
     >
       <Button
-        className="absolute right-0 top-0 bg-transparent hover:bg-transparent text-red-500 hover:text-red-400"
+        className="absolute right-0 top-0 bg-transparent text-red-500 hover:bg-transparent hover:text-red-400"
         onClick={() => onDelete(index, refId, questionPoints)}
       >
         <X />
@@ -70,12 +70,12 @@ const EditableOpenQuestion = ({
               id="editableQuestionTime"
               contentEditable
               suppressContentEditableWarning={true}
-              className=" max-w-14 ml-2"
+              className="ml-2 max-w-14"
               onInput={(e) => {
-                disableTextInInput(e)
-                const newTime = parseInt(e.currentTarget.innerText)
-                setQuestionTime(newTime)
-                onInput(refId, newTime, questionPoints)
+                disableTextInInput(e);
+                const newTime = parseInt(e.currentTarget.innerText);
+                setQuestionTime(newTime);
+                onInput(refId, newTime, questionPoints);
               }}
             >
               {question.time}
@@ -84,7 +84,7 @@ const EditableOpenQuestion = ({
           </div>
         </div>
         <div className="relative">
-          {image && image !== 'undefined' ? (
+          {image && image !== "undefined" ? (
             <Image
               alt="quizphoto"
               src={image}
@@ -93,20 +93,20 @@ const EditableOpenQuestion = ({
               className=""
             />
           ) : (
-            <div className="w-full bg-slate-950 col-span-2  border p-8">
+            <div className="col-span-2 w-full border bg-slate-950 p-8">
               <p>Zdjęcie</p>
             </div>
           )}
-          <div className="absolute bg-red-500 flex p-1   right-0 top-0">
+          <div className="absolute right-0 top-0 flex bg-red-500 p-1">
             <Input
               type="file"
               name={`file-${index}`}
               id={`imgInput${index}`}
-              className="hidden "
+              className="hidden"
               onChange={(e) => showImage(e)}
             />
             <label htmlFor={`imgInput${index}`} className="cursor-pointer">
-              <Pen size={20} />{' '}
+              <Pen size={20} />{" "}
             </label>
           </div>
         </div>
@@ -116,12 +116,12 @@ const EditableOpenQuestion = ({
             id="editableQuestionPoints"
             contentEditable
             suppressContentEditableWarning={true}
-            className=" max-w-14"
+            className="max-w-14"
             onInput={(e) => {
-              disableTextInInput(e)
-              const newPoints = parseInt(e.currentTarget.innerText)
-              setQuestionPoints(newPoints)
-              onInput(refId, questionTime, newPoints)
+              disableTextInInput(e);
+              const newPoints = parseInt(e.currentTarget.innerText);
+              setQuestionPoints(newPoints);
+              onInput(refId, questionTime, newPoints);
             }}
           >
             {question.points}
@@ -129,29 +129,29 @@ const EditableOpenQuestion = ({
         </div>
       </div>
       <p
-        className="text-xl break-words max-w-full"
+        className="max-w-full break-words text-xl"
         id="editableQuestionTitle"
         contentEditable
         suppressContentEditableWarning={true}
       >
         {question.title}
       </p>
-      <div className=" flex flex-wrap gap-3 justify-center">
+      <div className="flex flex-wrap justify-center gap-3">
         {answears.map((answear: any, index: number) => (
           <EditableOpenAnswear answear={answear} key={index} />
         ))}
       </div>
       <Button
-        className="w-full bg-slate-950 col-span-2 hover:bg-slate-800 border py-8 relative"
+        className="relative col-span-2 w-full border bg-slate-950 py-8 hover:bg-slate-800"
         onClick={() => {
-          addNewAnswear()
+          addNewAnswear();
         }}
       >
         <Plus className="absolute left-1/2 top-1/2" />
         <Lightbulb />
       </Button>
     </div>
-  )
-}
+  );
+};
 
-export default EditableOpenQuestion
+export default EditableOpenQuestion;

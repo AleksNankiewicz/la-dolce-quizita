3e929aae6ac36068a75b31fea1e0f16dea5a0c46
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-06-03 22:25:11 +0200

    try-5

--- src/components/misc/HomeSlider.tsx
+++ src/components/misc/HomeSlider.tsx
@@ -1,43 +1,28 @@
-'use client'
-import {
-  QuizesPlayedProps,
-  UserProps,
-  quizProps,
-  sessionUserProps,
-} from '@/types/data'
-import React, { useEffect, useRef, useState } from 'react'
-import Slider from 'react-slick'
-import SmallQuizBlock from '../layouts/blocks/SmallQuizBlock'
+"use client";
 
-import EditQuizButton from '../layouts/EditQuizButton'
-import Image from 'next/image'
-import Link from 'next/link'
-//import Marquee from 'react-fast-marquee'
-import { formatNumber } from '@/lib/utils'
-import { UserRound } from 'lucide-react'
-import { useSession } from 'next-auth/react'
-import { getUserByEmail } from '@/lib/actions'
-import SliderQuiz from './SliderQuiz'
-import Loading from '@/app/loading'
-import { ThreeDots } from 'react-loader-spinner'
+import React, { useEffect, useState } from "react";
 
-import { useKeenSlider } from 'keen-slider/react'
-import 'keen-slider/keen-slider.min.css'
-import { QuizWithQuestions } from '@/types/extended'
+import SliderQuiz from "./SliderQuiz";
+
+import { ThreeDots } from "react-loader-spinner";
+
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import { QuizWithQuestions } from "@/types/extended";
 
 const HomeSlider = ({ quizes }: { quizes: QuizWithQuestions[] }) => {
-  const [email, setEmail] = useState<string | undefined>()
+  const [email, setEmail] = useState<string | undefined>();
 
-  const animation = { duration: 20000, easing: (t: number) => t }
-  const [loading, setLoading] = useState(true)
+  const animation = { duration: 20000, easing: (t: number) => t };
+  const [loading, setLoading] = useState(true);
   const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
     loop: true,
     drag: true,
-    mode: 'free',
-    renderMode: 'precision',
+    mode: "free",
+    renderMode: "precision",
 
     breakpoints: {
-      '(min-width: 768px)': {
+      "(min-width: 768px)": {
         slides: {
           perView: 4,
           spacing: 13,
@@ -50,23 +35,23 @@ const HomeSlider = ({ quizes }: { quizes: QuizWithQuestions[] }) => {
     },
 
     created(s) {
-      s.moveToIdx(5, true, animation)
+      s.moveToIdx(5, true, animation);
     },
     updated(s) {
-      s.moveToIdx(s.track.details.abs + 5, true, animation)
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
     },
     animationEnded(s) {
-      s.moveToIdx(s.track.details.abs + 5, true, animation)
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
     },
-  })
+  });
 
   useEffect(() => {
-    setLoading(false)
-  }, [loading])
+    setLoading(false);
+  }, [loading]);
 
   if (loading) {
     return (
-      <div className="relative w-full mx-2 h-[180px] sm:h-[240px] md:h-[200px] lg:h-[280px] flex justify-center items-center md:col-span-4 col-span-2">
+      <div className="relative col-span-2 mx-2 flex h-[180px] w-full items-center justify-center sm:h-[240px] md:col-span-4 md:h-[200px] lg:h-[280px]">
         <ThreeDots
           visible={true}
           height="40"
@@ -78,21 +63,21 @@ const HomeSlider = ({ quizes }: { quizes: QuizWithQuestions[] }) => {
           wrapperClass=""
         />
       </div>
-    )
+    );
   }
 
   return (
-    <div className="w-full col-span-2 md:col-span-4">
-      <div ref={sliderRef} className="keen-slider ">
-        <div className="absolute w-16 -left-2 top-0 h-full  bg-gradient-to-r from-slate-950 to-black/0  z-20"></div>
-        <div className="absolute w-20 -right-2 top-0 h-full  bg-gradient-to-l from-slate-950 to-black/0  z-20"></div>
+    <div className="col-span-2 w-full md:col-span-4">
+      <div ref={sliderRef} className="keen-slider">
+        <div className="absolute -left-2 top-0 z-20 h-full w-16 bg-gradient-to-r from-slate-950 to-black/0"></div>
+        <div className="absolute -right-2 top-0 z-20 h-full w-20 bg-gradient-to-l from-slate-950 to-black/0"></div>
 
         {quizes.map((quiz: QuizWithQuestions) => (
           <SliderQuiz quiz={quiz} email={email} key={quiz.slug} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSlider
+export default HomeSlider;

744e0a64dde5a4720be70e12ac84a73c8e72e2b4
AleksNankiewicz <151682321+AleksNankiewicz@users.noreply.github.com>
2024-06-04 12:57:36 +0200

    try-9

--- src/components/pages/editAiQuiz/EditAiQuizForm.tsx
+++ src/components/pages/editAiQuiz/EditAiQuizForm.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -8,31 +8,31 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card'
-import { Textarea } from '@/components/ui/textarea'
-import { Button } from '@/components/ui/button'
-import { generateAiQuiz } from '@/lib/actions/generateAiQuiz'
-import { ExtendedQuiz } from '@/types/extended'
-import EditQuizForm from '@/components/pages/editQuiz/EditQuizForm'
+} from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { generateAiQuiz } from "@/lib/actions/generateAiQuiz";
+import { ExtendedQuiz } from "@/types/extended";
+import EditQuizForm from "@/components/pages/editQuiz/EditQuizForm";
 
 const EditAiQuizForm = () => {
-  const [prompt, setPrompt] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [quiz, setQuiz] = useState<ExtendedQuiz | null>(null)
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [quiz, setQuiz] = useState<ExtendedQuiz | null>(null);
   const handleSubmit = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const generatedQuizString = await generateAiQuiz(prompt)
-      console.log(generatedQuizString)
-      if (!generatedQuizString) return
-      const generatedQuiz = JSON.parse(generatedQuizString) as ExtendedQuiz
-      console.log(generatedQuiz)
-      setLoading(false)
-      setQuiz(generatedQuiz)
+      const generatedQuizString = await generateAiQuiz(prompt);
+      console.log(generatedQuizString);
+      if (!generatedQuizString) return;
+      const generatedQuiz = JSON.parse(generatedQuizString) as ExtendedQuiz;
+      console.log(generatedQuiz);
+      setLoading(false);
+      setQuiz(generatedQuiz);
     } catch (error) {
-      console.error('Error generating quiz:', error)
+      console.error("Error generating quiz:", error);
     }
-  }
+  };
 
   return (
     <>
@@ -55,9 +55,11 @@ const EditAiQuizForm = () => {
           </Button>
         </CardFooter>
       </Card>
-      {quiz && <EditQuizForm initialQuiz={quiz} />}
-    </>
-  )
-}
 
-export default EditAiQuizForm
+      {/* no user temp */}
+      {quiz && <EditQuizForm initialQuiz={quiz} userId="noUser" />}
+    </>
+  );
+};
+
+export default EditAiQuizForm;
